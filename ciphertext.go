b82fa00c4e7f5b749ea63b3a91d9d8b63bc6076a
TinSoldier6 <jaygade@yahoo.com>
2015-04-07 20:14:40 -0700

    Editing Secure[Reader|Writer] and friends.

--- ciphertext.go
+++ ciphertext.go
@@ -1,45 +1,81 @@
 package main
 
 import (
-	"golang.org/x/crypto/nacl/box"
+	"bytes"
+	"crypto/rand"
+	"encoding/binary"
 	"io"
+
+	"golang.org/x/crypto/nacl/box"
+	"golang.org/x/crypto/nacl/secretbox"
 )
 
 const (
-	keySize   = 32
-	nonceSize = 24
+	keySize    = 32
+	nonceSize  = 24
+	msgLenSize = 2
+	overhead   = nonceSize + box.Overhead
+	bufSize    = 32768 + overhead
 )
 
-type cipherText struct {
-	txKey    *[keySize]byte
-	rxKey    *[keySize]byte
-	contents []byte
-}
-
+// SecureReader implements NaCl encryption over an io.Reader.
 type SecureReader struct {
-	r io.Reader
-	c cipherText
+	key *[keySize]byte
+	r   io.Reader
 }
 
-// NewSecureReader instantiates a new SecureReader
-func NewSecureReader(r io.Reader, priv, pub *[32]byte) SecureReader {
-	return nil
+// NewSecureReader instantiates a new SecureReader.
+func NewSecureReader(r io.Reader, priv, pub *[keySize]byte) SecureReader {
+	s := SecureReader{r: r}
+	box.Precompute(s.key, pub, priv)
+	return s
 }
 
 func (s SecureReader) Read(p []byte) (int, error) {
 	return 0, nil
 }
 
-type secureWriter struct {
-	w io.Writer
-	c cipherText
+// SecureWriter implements NaCl encryption over an io.Reader.
+type SecureWriter struct {
+	key *[keySize]byte
+	w   io.Writer
 }
 
-// NewSecureWriter instantiates a new SecureWriter
-func NewSecureWriter(w io.Writer, priv, pub *[32]byte) SecureWriter {
-	return nil
+// NewSecureWriter instantiates a new SecureWriter.
+func NewSecureWriter(w io.Writer, priv, pub *[keySize]byte) SecureWriter {
+	s := SecureWriter{w: w}
+	box.Precompute(s.key, pub, priv)
+	return s
 }
 
 func (s SecureWriter) Write(p []byte) (int, error) {
-	return 0, nil
+	m := packMessage(p)
+	out := make([]byte, nonceSize, nonceSize+len(m))
+	var nonce [nonceSize]byte
+
+	_, err := io.ReadFull(rand.Reader, nonce[:])
+	if err != nil {
+		return 0, err
+	}
+
+	copy(out, nonce[:])
+
+	return s.w.Write(secretbox.Seal(out, m, &nonce, s.key))
+}
+
+// packMessage prepends a slice's length to the message.
+func packMessage(m []byte) []byte {
+	l := len(m)
+	out := bytes.NewBuffer(make([]byte, 0, msgLenSize))
+	binary.Write(out, binary.LittleEndian, int16(l))
+	binary.Write(out, binary.LittleEndian, m)
+	return out.Bytes()
+}
+
+// unpackMessage extracts a length and a message from a previously packed message.
+func unpackMessage(m []byte) (int, []byte) {
+	out := bytes.NewBuffer(m)
+	var l int16
+	binary.Read(out, binary.LittleEndian, l)
+	return int(l), out.Bytes()
 }
